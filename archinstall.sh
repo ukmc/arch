c301c526f1f16cdc92771ce3d98eddef87cbe0ef
ukmc <ukmc.j0@gmail.com>
2020-10-08 14:42:34 +0300

    update

--- archinstall.sh
+++ archinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Install packages - assumes multilib enabled
-sudo pacman -Syyu mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver xorg sddm plasma python3 python-pip code libreoffice kitty transmission-qt firefox calibre leafpad nomacs okular flameshot dolphin vlc openssh latte-dock discord ntfs-3g htop neofetch adobe-source-han-sans-jp-fonts noto-fonts-emoji cronie rsync unrar wget docker bash-completion net-tools krita kdenlive ark
+sudo pacman -Syyu mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver xorg sddm plasma python3 python-pip code libreoffice kitty transmission-qt firefox calibre leafpad nomacs okular flameshot dolphin vlc openssh latte-dock discord ntfs-3g htop neofetch adobe-source-han-sans-jp-fonts noto-fonts-emoji cronie rsync unrar wget docker bash-completion net-tools krita kdenlive ark arch-audit p7zip
 
 #Enable installed services
 sudo systemctl enable sddm fstrim.timer docker
