a702d2aac1cde6b887096e224ddab2843a15c9f4
ukmc <ukmc.j0@gmail.com>
2020-11-18 14:40:20 +0300

    Replaced code with vsc-bin added google-keep

--- archinstall.sh
+++ archinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Install packages - assumes multilib enabled
-sudo pacman -Syyu mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver xorg sddm plasma python3 python-pip code libreoffice kitty transmission-qt firefox calibre leafpad nomacs okular flameshot dolphin vlc openssh latte-dock discord ntfs-3g htop neofetch adobe-source-han-sans-jp-fonts noto-fonts-emoji cronie rsync unrar wget docker bash-completion net-tools krita kdenlive ark arch-audit p7zip
+sudo pacman -Syyu mesa lib32-mesa xf86-video-amdgpu vulkan-radeon lib32-vulkan-radeon mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver xorg sddm plasma python3 python-pip libreoffice kitty transmission-qt firefox calibre leafpad nomacs okular flameshot dolphin vlc openssh latte-dock discord ntfs-3g htop neofetch adobe-source-han-sans-jp-fonts noto-fonts-emoji cronie rsync unrar wget docker bash-completion net-tools krita kdenlive ark arch-audit p7zip
 
 #Enable installed services
 sudo systemctl enable sddm fstrim.timer docker
@@ -20,7 +20,7 @@ cd yay-git
 makepkg -si PKGBUILD
 
 #Install Aur packages
-yay -Syyu ttf-ms-fonts timeshift hakuneko-desktop
+yay -Syyu ttf-ms-fonts timeshift hakuneko-desktop google-keep-nativefier visual-studio-code-bin
 
 #Set env
 echo setxkbmap tr >> ~/.bashrc
